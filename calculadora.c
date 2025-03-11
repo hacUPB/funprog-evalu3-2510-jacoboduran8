963537a7d5582532d5ed8d897efe4bce08cb5323
jacoboduran8 <jacobo.durana@upb.edu.co>
2025-03-11 14:36:29 +0000

    Cambios calculadora

---
+++ calculadora.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int opcion;
+    int a, b;
+
+    printf("1. Suma\n 2. Resta\n 3. Multiplicación\n 4. División\n");
+    scnaf("&d" , &opcion);
+    printf("Ingrese los dos números con los que quiera realizar la operación a,b");
+    scnaf("&d" , &a, &b);
+
+    
+
+
+
+    
