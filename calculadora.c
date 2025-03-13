c16f303d7d8fb2d4f606194db3d43720510dc28c
Jacobo <jacoboduranaristizabal@gmail.com>
2025-03-13 09:42:52 -0500

    Se finalizó la calculadora corregida

--- calculadora.c
+++ calculadora.c
@@ -2,44 +2,57 @@
 
 int main() {
     int opcion;
-    float a, b;
+    float a, b, Suma, Resta, Mult, Div;
+    do
+    {
+    
+        printf("1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n 5. Salir\n");
+        scanf("%d" , &opcion);
+        if (opcion == 5)
+        {
+            break;
+        }
+        printf("Ingrese el primer numero que quiere usar: ");
+        scanf("%f" , &a);
+        printf("Ingrese el segundo numero: ");
+        scanf("%f" , &b);
 
-    printf("1. Suma\n 2. Resta\n 3. Multiplicación\n 4. División\n");
-    scnaf("&d" , &opcion);
-    printf("Ingrese los dos números con los que quiera realizar la operación a,b");
-    scnaf("&f" , &a, &b);
+        
 
-    switch (opcion) {
-        case "1":
+        switch (opcion) {
+        case 1:
             Suma = a + b;
             printf("Eligió suma y el reultado es: %.2f\n", Suma);
             break;
         
-        case "2":
+        case 2:
             Resta = a - b;
             printf("Eligio resta y el resultado es: %.2f\n", Resta);
             break;
         
-        case "3":
+        case 3:
             Mult = a * b;
-            printf("Eligio multiplicación y el resultado es: %.2f\n", Mult);
+            printf("Eligio multiplicacion y el resultado es: %.2f\n", Mult);
             break;
 
-        case "4"
-            if (b=0)
-                printf("Error matemático");
-            else
+        case 4:
+            if (b==0)
+                printf("Error matematico");
+            else 
             {
                 Div = a / b; 
-                printf("Eligio división y el resultado es: %.2f\n", Div);
-                return;
+                printf("Eligio division y el resultado es: %.2f\n", Div);
             }
             break;
 
         default:
-    }
+            printf("Operacion no valida\n");
+            break;
+        }
+    } while (opcion != 5);
+        printf("Terminaste tus operaciones por hoy\n");
 
-    return 0;
+ return 0;
 }
             
 
