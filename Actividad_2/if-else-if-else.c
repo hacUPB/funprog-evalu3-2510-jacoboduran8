a0245bb8a226f9c10bbcc7a74501816d62fb631b
Jacobo <jacoboduranaristizabal@gmail.com>
2025-03-19 22:05:50 -0500

    Se agrego la Actividad_2 al repositorio

---
+++ Actividad_2/if-else-if-else.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int nota;
+
+    printf("Ingrese la calificación: ");
+    scanf("%d", nota);
+
+    // Evaluar la calificación con if-else if-else
+    if (nota == 0 & nota == 1) {
+        printf("Insuficiente\n");
+    } else if (nota == 2) {
+        printf("Deficiente\n");
+    } else if (nota == 3) {
+        printf("Aceptable\n");
+    } else if (nota == 4) {
+        printf("Notable\n");
+    } else if (nota == 5) {
+        printf("Sobresaliente\n");
+    } else {
+        printf("Calificación no válida.\n");
+    }
+
+    return 0;
+}
